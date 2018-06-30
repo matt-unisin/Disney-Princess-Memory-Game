98ed0bec6b725e0091e6b8247ae9c119775d916c
UnisiN <matt.unisin@gmail.com>
2018-06-30 19:10:50 -0400

    added seperate game setup file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Just a basic memory card game with Disney themed graphics. It has 3 skill levels (beginner, intermediate and advanced), with 6 levels per skill level. Each level has it's own easily adjusted parameters using a timer and card flip counter.
+
+Go to the gamesetup.js file to edit game parameters for medals and maximum amounts of time and card flips
---
+++ game.setup.js
@@ -0,0 +1,16 @@
+//////////////////////////////////
+// SET UP INITIAL VARIABLES
+//////////////////////////////////
+
+// Set maximum flips for each medal
+
+var goldMax = 45;
+var silverMax = 60;
+var bronzeMax = 75;
+
+// Set up the max flips, which will automatically
+// Reduce with each level and selected skill level
+// Change if the game is too easy or difficult
+
+var setMaxFlips = '100'; // Max card flips (flipCounter)
+var setMaxTime = '100'; // Max time (myTimer)
