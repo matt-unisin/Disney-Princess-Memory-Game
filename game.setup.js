b917af4cc7804a138cc68e07bc6cea559c43dd27
UnisiN <matt.unisin@gmail.com>
2018-07-01 01:57:56 -0400

    styled sidebar, cleaned code

--- game.setup.js
+++ game.setup.js
@@ -1,11 +1,11 @@
 // Set maximum flips for each medal
 
-var goldMax = 60;
-var silverMax = 75;
-var bronzeMax = 90;
+var goldMax = 50;
+var silverMax = 65;
+var bronzeMax = 80;
 
 /*
-Set up the max flips, which will automatically reduce with each level and selected skill level. Change if the game is too easy or too difficult.
+Set up the max flips, which will automatically reduce with each level and change with every selected skill level (starting with beginner, level 1). Change these values if the game is too easy or too difficult.
 */
 
 var setMaxFlips = '110';
