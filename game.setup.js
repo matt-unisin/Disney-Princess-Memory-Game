0668c954cafb6fb241c6c2024e2aa7ae2c74ea98
UnisiN <matt.unisin@gmail.com>
2018-06-30 22:05:33 -0400

    added game setup

--- game.setup.js
+++ game.setup.js
@@ -1,16 +1,12 @@
-//////////////////////////////////
-// SET UP INITIAL VARIABLES
-//////////////////////////////////
-
 // Set maximum flips for each medal
 
-var goldMax = 45;
-var silverMax = 60;
-var bronzeMax = 75;
+var goldMax = 60;
+var silverMax = 75;
+var bronzeMax = 90;
 
-// Set up the max flips, which will automatically
-// Reduce with each level and selected skill level
-// Change if the game is too easy or difficult
+/*
+Set up the max flips, which will automatically reduce with each level and selected skill level. Change if the game is too easy or too difficult.
+*/
 
-var setMaxFlips = '100'; // Max card flips (flipCounter)
-var setMaxTime = '100'; // Max time (myTimer)
+var setMaxFlips = '110';
+var setMaxTime = '110';
