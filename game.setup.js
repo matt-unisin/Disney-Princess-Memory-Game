4ff5891f03ab95470eb19681c57a14c9b69fbf1e
UnisiN <matt.unisin@gmail.com>
2018-07-23 06:39:29 -0400

    added loading screen

--- game.setup.js
+++ game.setup.js
@@ -5,7 +5,9 @@ var silverMax = 65;
 var bronzeMax = 80;
 
 /*
-Set up the max flips, which will automatically reduce with each level and change with every selected skill level (starting with beginner, level 1). Change these values if the game is too easy or too difficult.
+Set up the max flips, which will automatically reduce with each level
+and change with every selected skill level (starting with beginner, level 1). 
+Change these values if the game is too easy or too difficult.
 */
 
 var setMaxFlips = '110';
